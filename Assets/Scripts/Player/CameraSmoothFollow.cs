00fa6bf491136e25c3f9a5b5d850161e75cad8dd
Allen Brower <allenmbrower@gmail.com>
2016-12-10 02:00:20 -0500

    Added a smooth follow camera Cleaned up some code

---
+++ Assets/Scripts/Player/CameraSmoothFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmoothFollow : MainBehaviour {
+
+	public Transform FollowTarget;
+	public float FollowSpeed = 5.0f;
+	public Vector3 offset = Vector3.zero;
+
+	protected override void FixedGameUpdate(){
+
+		transform.position = Vector3.Lerp(transform.position, FollowTarget.position + offset, FollowSpeed);
+
+	}
+
+}
--- Assets/Scripts/Player/ModelLook.cs
+++ Assets/Scripts/Player/ModelLook.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ModelLook : MonoBehaviour {
+public class ModelLook : MainBehaviour {
 
 	public PlayerMovement MyMovement;
 
@@ -15,13 +15,8 @@ public class ModelLook : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () {
-
-		/*LookAt = new Vector3(MyMovement.PlayerInput.x * 10f, 0f, MyMovement.PlayerInput.y * 10f);
-		float angle = Vector3.Angle(transform.position, transform.InverseTransformDirection(LookAt));
-		transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.MoveTowards(transform.eulerAngles.y, angle, 90.0f * Time.deltaTime), transform.eulerAngles.z);
-		*/
-
+	protected override void FixedGameUpdate() {
+		
 		if(MyMovement.PlayerInput != Vector2.zero)
 			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(MyMovement.PlayerInput.x, 0, MyMovement.PlayerInput.y)), 5f * Time.deltaTime);
 
