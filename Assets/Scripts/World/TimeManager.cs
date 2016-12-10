f4df9bd0f9c22d99a17557c52555a98220947a89
Allen Brower <allenmbrower@gmail.com>
2016-12-10 15:42:02 -0500

    Day night system!!!!

--- Assets/Scripts/World/TimeManager.cs
+++ Assets/Scripts/World/TimeManager.cs
@@ -14,6 +14,18 @@ public class TimeManager : MainBehaviour {
 		}
 	}
 
+	public Transform Light;
+	public GameObject Sun;
+	public GameObject Moon;
+
+	public float RelativeStartTime = 12f;
+
+	void Start(){
+
+		CurrentDayTime = (RelativeStartTime / 24f) * DayLength;
+
+	}
+
 	protected override void FixedGameUpdate(){
 
 		CurrentDayTime += Time.fixedDeltaTime;
@@ -22,6 +34,22 @@ public class TimeManager : MainBehaviour {
 			CurrentDayTime = 0.0f;
 			CurrentDay++;
 		}
+
+		Light.transform.eulerAngles = new Vector3(0,0,(CurrentDayTime / DayLength) * 360f);
+
+		if(CurrentTime <= 6f){
+			Sun.SetActive(false);
+			Moon.SetActive(true);
+		}
+		else if(CurrentTime > 6f && CurrentTime <= 18f){
+			Sun.SetActive(true);
+			Moon.SetActive(false);
+		}
+		else if(CurrentTime > 18f){
+			Sun.SetActive(false);
+			Moon.SetActive(true);
+		}
+
 
 	}
 
